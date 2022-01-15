a50da70a4dee8c7f784b6f311769ed8eb8f7d28b
Tosh Velaga <toshvelaga@gmail.com>
2022-01-15 15:25:39 -0700

    add Config to puppeteerRecord

--- puppeteerRecord.js
+++ puppeteerRecord.js
@@ -1,10 +1,21 @@
 const puppeteer = require('puppeteer')
 const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder')
 
+const Config = {
+  followNewTab: true,
+  fps: 25,
+  videoFrame: {
+    width: 1024,
+    height: 768,
+  },
+  aspectRatio: '4:3',
+  recordDurationLength: 10,
+}
+
 ;(async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  const recorder = new PuppeteerScreenRecorder(page)
+  const recorder = new PuppeteerScreenRecorder(page, Config)
   await recorder.start('./report/video/spotify.mp4') // supports extension - mp4, avi, webm and mov
   await page.goto('https://open.spotify.com/episode/033bxIFePCK1JIy3wiBxBL')
   await recorder.stop()
