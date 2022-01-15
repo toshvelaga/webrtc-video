bb8afe7ba2151a530573e2b4b5bafd72ca13b5a7
Tosh Velaga <toshvelaga@gmail.com>
2022-01-14 22:11:40 -0700

    open pupeteer to take a screenshot

--- puppeteer.js
+++ puppeteer.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer')
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
-  await page.goto('https://example.com')
+  await page.goto('https://ohmystream.co/studio')
   await page.screenshot({ path: 'example.png' })
 
   await browser.close()
