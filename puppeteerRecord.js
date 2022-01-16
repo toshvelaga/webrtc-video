14376a252e65035c0309dc22a457f88790f257f0
Tosh Velaga <toshvelaga@gmail.com>
2022-01-15 19:53:23 -0700

    edit puppeteerRecord

--- puppeteerRecord.js
+++ puppeteerRecord.js
@@ -13,11 +13,11 @@ const Config = {
 }
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
   const recorder = new PuppeteerScreenRecorder(page, Config)
-  await recorder.start('./report/video/spotify.mp4') // supports extension - mp4, avi, webm and mov
-  await page.goto('https://open.spotify.com/episode/033bxIFePCK1JIy3wiBxBL')
+  await recorder.start('./report/video/video.mp4') // supports extension - mp4, avi, webm and mov
+  await page.goto('https://video-meeting-socket.herokuapp.com/tosh')
   await recorder.stop()
   await browser.close()
 })()
