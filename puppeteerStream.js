5b1f1f76d73de35d20b4f8ddf6d879e350e9f4a2
Tosh Velaga <toshvelaga@gmail.com>
2022-02-17 21:29:24 -0700

    turn pupeteerStream into a route

--- puppeteerStream.js
+++ puppeteerStream.js
@@ -1,18 +1,26 @@
 const { launch, getStream } = require('puppeteer-stream')
 const fs = require('fs')
+const express = require('express')
+const router = express.Router()
 
 // node arguments are present from the third position going forward.
 const args = process.argv.slice(2)
 const file = fs.createWriteStream(`./reports/videos/${args[0]}.mp4`)
 
-async function puppeteerStream() {
+require('dotenv').config()
+
+// OLD DOCS = https://legacydocs.hubspot.com/docs/methods/contacts/create_contact
+
+// NEW DOCS = https://developers.hubspot.com/docs/api/crm/contacts
+
+const puppeteerStream = async (url) => {
   const browser = await launch({
     executablePath:
       '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   })
 
   const page = await browser.newPage()
-  await page.goto('http://localhost:8000/demo')
+  await page.goto(url)
   await page.waitForSelector('button')
   await page.click('button')
 
@@ -26,4 +34,10 @@ async function puppeteerStream() {
   file.close()
 }
 
-puppeteerStream()
+router.post('/api/record', async (req, res) => {
+  const { url } = req.body
+  puppeteerStream(url)
+  return res.status(201).send('recording')
+})
+
+module.exports = router()
